24e1f5ac59fe52167bdd574ea6879211ff5f2041
pratik753 <60500551+pratik753@users.noreply.github.com>
2022-11-05 14:26:48 +0530

    bug fix

--- weather.js
+++ weather.js
@@ -18,41 +18,40 @@ let weather = {
     )
       .then((response) => {
         if (!response.ok) {
-          alert("No weather found.");
+          alert("Enter City");
           throw new Error("No weather found.");
         }
         return response.json();
       })
       .then((data) => this.displayWeather(data));
   },
-displayWeather: function(data)
-{
- const{name}=data;
- const{ icon,description}=data.weather[0];
-//  const{icon,description}=data.main;
- const{temp,humidity}=data.main;
- const {speed}=data.wind;
-//  console.log(name,icon,description,temp,humidity,speed)
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    //  const{icon,description}=data.main;
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    //  console.log(name,icon,description,temp,humidity,speed)
 
- document.querySelector(".city").innerText="Weather in" +name;
- document.querySelector(".icon").src =
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + ".png";
- document.querySelector(".description").innerText=description;
- document.querySelector(".temp").innerText=temp + "C" ;
- document.querySelector(".humidity").innerText="Humidity: " + humidity + "%";
- document.querySelector(".wind").innerText="Wind speed"+ speed + " km/h" ;
- document.querySelector(".weather").classList.remove("loading");
- console.log(speed);
- document.body.style.backgroundImage =
-   "url('https://source.unsplash.com/1600x900/?" + name + "')";
-},
-search: function () {
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    console.log(speed);
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
   },
 };
-document.querySelector(".search button").addEventListener("click",function(){
-    weather.search();
-
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
 });
 document
   .querySelector(".search-bar")
@@ -62,5 +61,5 @@ document
     }
   });
 
-weather.fetchWeather("Denver");
+weather.fetchWeather("City Name");
 // weather.displayWeather(data);
